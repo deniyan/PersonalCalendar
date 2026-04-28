c17bb27bc2d0f5efd5c0eddab2480b03e651e37b
deniyan <deniyanst@gmail.com>
2026-04-28 22:53:30 +0300

    Добавяне на реално запазване в xml файл чрез командата save

--- src/bg/tu_varna/sit/f24621709/personalcalendar/repository/XmlCalendarRepository.java
+++ src/bg/tu_varna/sit/f24621709/personalcalendar/repository/XmlCalendarRepository.java
@@ -1,6 +1,11 @@
 package bg.tu_varna.sit.f24621709.personalcalendar.repository;
 
 import bg.tu_varna.sit.f24621709.personalcalendar.model.Calendar;
+import bg.tu_varna.sit.f24621709.personalcalendar.model.Event;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class XmlCalendarRepository implements CalendarRepository {
 
@@ -12,6 +17,24 @@ public class XmlCalendarRepository implements CalendarRepository {
 
     @Override
     public void save(String path, Calendar calendar) {
-        System.out.println("Saving to XML: " + path);
+        StringBuilder xml = new StringBuilder();
+        xml.append("<calendar>\n");
+
+        for (Event event : calendar.getEventsList()){
+            xml.append("\t<event>\n");
+            xml.append("\t\t<date>" + event.getDate() + "</date>\n");
+            xml.append("\t\t<starttime>" + event.getStarttime() + "</starttime>\n");
+            xml.append("\t\t<endtime>" + event.getEndtime() + "</endtime>\n");
+            xml.append("\t\t<name>" + event.getName() + "</name>\n");
+            xml.append("\t\t<note>" + event.getNote() + "</note>\n");
+            xml.append("\t</event>\n");
+        }
+        xml.append("</calendar>");
+        //System.out.println("Saving to XML: " + path);
+        try {
+            Files.writeString(Path.of(path), xml.toString());
+        }catch (IOException e){
+            throw new RuntimeException("Error saving file: " + e.getMessage());
+        }
     }
 }
